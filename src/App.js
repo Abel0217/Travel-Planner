f8f2cc75246b749375c94fb7aa21eeb890a5a9e7
Abel0217 <abelstar10@gmail.com>
2024-11-26 13:57:45 -0500

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LoadScript } from '@react-google-maps/api';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useContext } from 'react';
 import NavBar from './Components/NavBar';
-import ItineraryList from './features/Itinerary/components/ItineraryList';
+import ItineraryView from './features/ItineraryView'; 
 import ItineraryForm from './features/Itinerary/components/ItineraryForm';
 import ItineraryDetails from './features/Itinerary/components/ItineraryDetails';
-import ItineraryOverview from './features/Itinerary/components/ItineraryOverview/ItineraryOverview'; 
+import ItineraryOverview from './features/Itinerary/components/ItineraryOverview/ItineraryOverview';
 import FlightsTab from './features/Itinerary/components/FlightsTab';
 import HotelTab from './features/Itinerary/components/HotelsTab';
 import RestaurantsTab from './features/Itinerary/components/RestaurantsTab';
 import TransportTab from './features/Itinerary/components/TransportTab';
 import ActivitiesTab from './features/Itinerary/components/ActivitiesTab';
-import ItineraryView from './features/ItineraryView'; 
-import Profile from './features/Profile'; 
+import Profile from './features/Profile';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
 import Reminders from './features/Notifications';
 import Expense from './features/Expense';
-import FriendsPage from './features/Friends/FriendsPage'; 
-import { AuthContextProvider } from './Contexts/AuthContext';
+import FriendsPage from './features/Friends/FriendsPage';
+import { AuthContextProvider, AuthContext } from './Contexts/AuthContext';
 import './App.css';
+
+// Private Wrapper for Authenticated Routes
+function PrivateWrapper({ children }) {
+  const { currentUser } = useContext(AuthContext);
+
+  if (!currentUser) {
+    // Redirect to login if user is not authenticated
+    return <Navigate to="/login" />;
+  }
+
+  return <>{children}</>;
+}
 
 function App() {
   return (
@@ -27,22 +38,34 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/itineraries-view" element={<ItineraryView />} />
-          <Route path="/itineraries/create" element={<ItineraryForm />} />
-          <Route path="/itineraries/:itineraryId" element={<ItineraryDetails />} />
-          <Route path="/itineraries/:itineraryId/overview" element={<ItineraryOverview />} />  
-          <Route path="/itineraries/:itineraryId/flights" element={<FlightsTab />} />
-          <Route path="/itineraries/:itineraryId/hotels" element={<HotelTab />} />
-          <Route path="/itineraries/:itineraryId/restaurants" element={<RestaurantsTab />} />
-          <Route path="/itineraries/:itineraryId/transport" element={<TransportTab />} />
-          <Route path="/itineraries/:itineraryId/activities" element={<ActivitiesTab />} />
+          {/* Public Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/profile" element={<Profile />} />  
-          <Route path="/notifications" element={<Reminders />} />
-          <Route path="/friends" element={<FriendsPage />} /> 
-          <Route path="/expenses" element={<Expense />} /> 
+
+          {/* Private Routes */}
+          <Route
+            path="/*"
+            element={
+              <PrivateWrapper>
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/itineraries-view" element={<ItineraryView />} />
+                  <Route path="/itineraries/create" element={<ItineraryForm />} />
+                  <Route path="/itineraries/:itineraryId" element={<ItineraryDetails />} />
+                  <Route path="/itineraries/:itineraryId/overview" element={<ItineraryOverview />} />
+                  <Route path="/itineraries/:itineraryId/flights" element={<FlightsTab />} />
+                  <Route path="/itineraries/:itineraryId/hotels" element={<HotelTab />} />
+                  <Route path="/itineraries/:itineraryId/restaurants" element={<RestaurantsTab />} />
+                  <Route path="/itineraries/:itineraryId/transport" element={<TransportTab />} />
+                  <Route path="/itineraries/:itineraryId/activities" element={<ActivitiesTab />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/notifications" element={<Reminders />} />
+                  <Route path="/friends" element={<FriendsPage />} />
+                  <Route path="/expenses" element={<Expense />} />
+                </Routes>
+              </PrivateWrapper>
+            }
+          />
         </Routes>
       </Router>
     </AuthContextProvider>
